fa2c7f7c4c4a9ad49daa0ceeba1dcb4b80095d78
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-11-23 22:03:56 +0100

    Start parsing command-line parameters

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+void display_help();
 
 typedef struct {
     char riff_header[4];
@@ -26,10 +30,62 @@ int main(int argc, char *argv[]){
     FILE* output_file;
     FILE* target_file;
 
+    /* default options */
+    uint32_t output_filename_arg_index = 0;
+    short channels = 1;
+    uint32_t sample_rate = 44100;
+    short bits_per_sample = 16;
+
+    static struct option long_options[] = {
+                                           {"help", no_argument, NULL, 'h'},
+                                           {"output", required_argument, NULL, 'o'},
+                                           {"channels", required_argument, NULL, 'c'},
+                                           {"sample-rate", required_argument, NULL, 's'},
+                                           {"bits-per-sample", required_argument, NULL, 'b'},
+                                           {NULL, 0, NULL, 0}
+    };
+
+    char *end;
+    int c;
+
+    while ((c = getopt_long(argc, argv, "ho:c:s:b:", long_options, NULL)) != -1) {
+        switch (c)
+            {
+            case 'h':
+                display_help();
+                exit(EXIT_FAILURE);
+            case 'o':
+                output_filename_arg_index = optind;
+                break;
+            case 'c':
+                channels = (int)strtol(optarg, &end, 10);
+                break;
+            case 's':
+                sample_rate = (int)strtol(optarg, &end, 10);
+                break;
+            case 'b':
+                bits_per_sample = (int)strtol(optarg, &end, 10);
+                break;
+            case '?':
+                fprintf(stderr,
+                        "%s: Use -h or --help to display options.\n", argv[0]);
+                exit(EXIT_FAILURE);
+            default: {}
+            }
+    }
+
+    /* TODO: catch parsing errors */
+
     target_file = fopen(argv[1], "rb");
     fseek(target_file, 0, SEEK_END);
     uint64_t target_file_size = ftell(target_file);
     fseek(target_file, 0, SEEK_SET);
 
+    const uint32_t wav_header_length = 44;
+
     return 0;
+}
+
+void display_help(){
+    /* Not implemented yet */
 }
